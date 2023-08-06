cf03e9c7cb8b141be4d79451b4d190069fe37615
Michael Georgiou <mikg25@gmail.com>
2023-08-06 13:24:42 +0100

    More Parallax work

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -42,14 +42,21 @@ const lightGreyTree11 = document.getElementById("light-grey-tree-11");
 const lightGreyTree12 = document.getElementById("light-grey-tree-12");
 const welcomeSign = document.getElementById("welcome-sign");
 
+const landing = document.getElementById("landing");
+const parallaxWoods = document.getElementById("parallax-woods");
+
+
 
 function parallaxEffect() {
+    const parallaxWoodsWidth = parallaxWoods.offsetWidth
+    const stopParallaxDynamicValue = ((parallaxWoodsWidth / 100) * 70)
 
-    if (window.innerHeight > window.innerWidth) {
-        return;
+    if (window.scrollY > stopParallaxDynamicValue) {
+        return
     }
-    let value = window.scrollY;
 
+    let value = window.scrollY;
+    
     let welcomeSignTopPosition = welcomeSign.style.top
     welcomeSignTopPosition = welcomeSignTopPosition.slice(0, -2);
 
@@ -80,28 +87,28 @@ function parallaxEffect() {
     blackTree8.style.left = value * 0.18 + "px";
     
 
-    greyGround.style.top = value * 1.007 + "px";
-    darkGreyTree1.style.top = value * 1.007 + "px";
-    darkGreyTree2.style.top = value * 1.007 + "px";
-    darkGreyTree3.style.top = value * 1.007 + "px";
+    greyGround.style.top = value * 1 + "px";
+    darkGreyTree1.style.top = value * 1 + "px";
+    darkGreyTree2.style.top = value * 1 + "px";
+    darkGreyTree3.style.top = value * 1 + "px";
     darkGreyTree1.style.left = value * -0.1 + "px";
     darkGreyTree2.style.left = value * -0.08 + "px";
     darkGreyTree3.style.left = value * -0.1 + "px";
 
-    darkGreyTree4.style.top = value * 1.007 + "px";
-    darkGreyTree5.style.top = value * 1.007 + "px";
-    darkGreyTree6.style.top = value * 1.007 + "px";
+    darkGreyTree4.style.top = value * 1 + "px";
+    darkGreyTree5.style.top = value * 1 + "px";
+    darkGreyTree6.style.top = value * 1 + "px";
     darkGreyTree4.style.left = value * 0.08 + "px";
     darkGreyTree5.style.left = value * 0.1 + "px";
     darkGreyTree6.style.left = value * 0.08 + "px";
 
 
-    greyTree1.style.top = value * 1.007 + "px";
-    greyTree2.style.top = value * 1.007 + "px";
-    greyTree3.style.top = value * 1.007 + "px";
-    greyTree4.style.top = value * 1.007 + "px";
-    greyTree5.style.top = value * 1.007 + "px";
-    greyTree6.style.top = value * 1.007 + "px";
+    greyTree1.style.top = value * 1 + "px";
+    greyTree2.style.top = value * 1 + "px";
+    greyTree3.style.top = value * 1 + "px";
+    greyTree4.style.top = value * 1 + "px";
+    greyTree5.style.top = value * 1 + "px";
+    greyTree6.style.top = value * 1 + "px";
     greyTree1.style.left = value * -0.05 + "px";
     greyTree2.style.left = value * -0.035 + "px";
     greyTree3.style.left = value * -0.05 + "px";
@@ -109,57 +116,41 @@ function parallaxEffect() {
     greyTree5.style.left = value * -0.05 + "px";
     greyTree6.style.left = value * 0.035 + "px";
     
-    greyTree7.style.top = value * 1.007 + "px";
-    greyTree8.style.top = value * 1.007 + "px";
-    greyTree9.style.top = value * 1.007 + "px";
+    greyTree7.style.top = value * 1 + "px";
+    greyTree8.style.top = value * 1 + "px";
+    greyTree9.style.top = value * 1 + "px";
     greyTree7.style.left = value * 0.05 + "px";
     greyTree8.style.left = value * 0.035 + "px";
     greyTree9.style.left = value * 0.05 + "px";
 
-    lightGreyGround.style.top = value * 0.96
-     + "px";
-    lightGreyTree1.style.top = value * 0.96
-     + "px";
-    lightGreyTree2.style.top = value * 0.96
-     + "px";
-    lightGreyTree3.style.top = value * 0.96
-     + "px";
-    lightGreyTree4.style.top = value * 0.96
-     + "px";
-    lightGreyTree5.style.top = value * 0.96
-     + "px";
+    lightGreyGround.style.top = value * 0.96 + "px";
+    lightGreyTree1.style.top = value * 0.96 + "px";
+    lightGreyTree2.style.top = value * 0.96 + "px";
+    lightGreyTree3.style.top = value * 0.96 + "px";
+    lightGreyTree4.style.top = value * 0.96 + "px";
+    lightGreyTree5.style.top = value * 0.96 + "px";
     lightGreyTree1.style.left = value * -0.02 + "px";
     lightGreyTree2.style.left = value * -0.018 + "px";
     lightGreyTree3.style.left = value * -0.02 + "px";
     lightGreyTree4.style.left = value * -0.018 + "px";
     lightGreyTree5.style.left = value * -0.02 + "px";
     
-    lightGreyTree6.style.top = value * 0.96
-     + "px";
-    lightGreyTree7.style.top = value * 0.96
-     + "px";
-    lightGreyTree8.style.top = value * 0.96
-     + "px";
-    lightGreyTree9.style.top = value * 0.96
-     + "px";
+    lightGreyTree6.style.top = value * 0.96 + "px";
+    lightGreyTree7.style.top = value * 0.96 + "px";
+    lightGreyTree8.style.top = value * 0.96 + "px";
+    lightGreyTree9.style.top = value * 0.96 + "px";
     lightGreyTree6.style.left = value * 0.018 + "px";
     lightGreyTree7.style.left = value * 0.02 + "px";
     lightGreyTree8.style.left = value * 0.018 + "px";
     lightGreyTree9.style.left = value * 0.02 + "px";
 
-    lightGreyTree10.style.top = value * 0.96
-     + "px";
-    lightGreyTree11.style.top = value * 0.96
-     + "px";
-    lightGreyTree12.style.top = value * 0.96
-     + "px";
+    lightGreyTree10.style.top = value * 0.96 + "px";
+    lightGreyTree11.style.top = value * 0.96 + "px";
+    lightGreyTree12.style.top = value * 0.96 + "px";
 
-
-
-    
     welcomeSign.style.top = value * 0.8 + "px";
-   
 }
 
 window.addEventListener("scroll", parallaxEffect);
+window.addEventListener("resize", parallaxEffect);
 
